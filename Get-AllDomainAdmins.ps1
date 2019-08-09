ee984fb584d808d9a3f13f58ea331e9786f3f5e1
idsu <idsu@graftonplc.net>
2019-08-09 11:01:43 +0100

    Initial Commit

---
+++ Get-AllDomainAdmins.ps1
@@ -0,0 +1,16 @@
+﻿
+Import-Module ActiveDirectory
+$Admins =@()
+$Count = 0
+$Domains = (Get-ADforest).domains
+$FilePath = "E:\Scripts\Ian\CSV\DA.csv"
+
+
+foreach($Domain in $Domains)
+{
+$PDCEmulator = get-addomain -Identity $Domain | Select -expandproperty pdcemulator
+$Admins = get-adgroupmember 'Domain Admins' -recursive -server $PDCEmulator | Select -expandproperty Name, DistinguishedName
+$Admins | export-csv $FilePath -append -NoTypeInformation
+
+}
+
